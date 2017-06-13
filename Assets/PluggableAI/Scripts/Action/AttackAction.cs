2f314e1e1575120848ecb70f62c4f21600e53bed
ChiLi_Lin <904981663@163.com>
2017-06-13 23:01:40 +0800

    Make AI Better Again
    Add Attack Fan-Shape Area ,Just Like Look Decision

--- Assets/PluggableAI/Scripts/Action/AttackAction.cs
+++ Assets/PluggableAI/Scripts/Action/AttackAction.cs
@@ -3,8 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu (menuName = "PluggableAI/Actions/Attack")]
-public class AttackAction : Action 
+[CreateAssetMenu(menuName = "PluggableAI/Actions/Attack")]
+public class AttackAction : Action
 {
     public override void Act(StateController controller)
     {
@@ -13,20 +13,59 @@ public class AttackAction : Action
 
     private void Attack(StateController controller)
     {
-        RaycastHit hit;
+        var enemyStats = controller.enemyStats;
 
-        Debug.DrawRay(controller.eyes.position, controller.eyes.forward.normalized * controller.enemyStats.attackMaxRange, Color.red);
-        
-        if (Physics.SphereCast(controller.eyes.position, controller.enemyStats.lookSphereCastRadius, controller.eyes.forward, out hit, controller.enemyStats.attackMaxRange)
-            && hit.collider.CompareTag("Player"))
+        Vector3 startLeft = controller.eyes.position - controller.eyes.right.normalized * enemyStats.lookSphereCastRadius / 2;
+        Vector3 startRight = controller.eyes.position + controller.eyes.right.normalized * enemyStats.lookSphereCastRadius / 2;
+
+        DebugDraw(controller, startLeft, startRight, Quaternion.Euler(0, 0, 0));
+        if (LookAround(controller, controller.eyes.forward) && controller.CheckIfCountDownElapsed(controller.enemyStats.attackRate))
         {
-            //过了时间间隔，可以发炮
-            if (controller.CheckIfCountDownElapsed(controller.enemyStats.attackRate))
+            controller.tankShooting.Fire(controller.enemyStats.attackForce, controller.enemyStats.attackRate);
+            return;
+        }
+
+        float attackAngle = enemyStats.attackAngle / 2;
+        float subAngle = attackAngle / enemyStats.attackAccurate;
+        //一个精确度就多两条对称的射线
+        for (int i = 0; i < enemyStats.attackAccurate; i++)
+        {
+            //两条射线
+            Quaternion eulerAngerLeft = Quaternion.Euler(0, -1 * subAngle * (i + 1), 0);
+            Quaternion eulerAngerRight = Quaternion.Euler(0, subAngle * (i + 1), 0);
+            DebugDraw(controller, startLeft, startRight, eulerAngerLeft);
+            DebugDraw(controller, startLeft, startRight, eulerAngerRight);
+
+            if (LookAround(controller, eulerAngerLeft * controller.eyes.forward) || LookAround(controller, eulerAngerRight * controller.eyes.forward))
             {
-                //射击的力度（长度），开始攻击的距离
                 controller.tankShooting.Fire(controller.enemyStats.attackForce, controller.enemyStats.attackRate);
+                return;
             }
         }
+
     }
+
+
+    //射出一球体的射线检测是否有Player
+    private bool LookAround(StateController controller, Vector3 lookForward)
+    {
+        RaycastHit hit;
+        if (Physics.SphereCast(controller.eyes.position, controller.enemyStats.lookSphereCastRadius, lookForward, out hit, controller.enemyStats.attackMaxRange))
+        {
+            if (hit.collider.CompareTag("Player"))
+                return true;
+            return false;
+        }
+        else
+            return false;
+    }
+
+    //调试信息：绘制两条射线代表扫描的范围，两线之内都会检测到
+    private void DebugDraw(StateController controller, Vector3 left, Vector3 right, Quaternion eulerAnger)
+    {
+        Debug.DrawRay(left, eulerAnger * controller.eyes.forward.normalized * controller.enemyStats.lookRange, Color.red);
+        Debug.DrawRay(right, eulerAnger * controller.eyes.forward.normalized * controller.enemyStats.lookRange, Color.red);
+    }
+
 
 }
--- Assets/PluggableAI/Scripts/State/EnemyStats.cs
+++ Assets/PluggableAI/Scripts/State/EnemyStats.cs
@@ -7,12 +7,14 @@ public class EnemyStats : ScriptableObject
 {
     public float moveSpeed = 1;                 //移动速度
     public float lookRange = 40f;               //检测敌人距离
-    public float lookSphereCastRadius = 1f;     //射出检测射线的射线半径
-    public float lookAngle = 90;                //射出射线角度
+    public float lookSphereCastRadius = 0f;     //射出检测射线的射线半径，越小性能越好
+    public float lookAngle = 90f;               //射出射线角度
     public int lookAccurate = 3;                //查找精度配合角度使用，0为只有一条向前的射线，
 
     public float attackMinRange = 5f;           //攻击最小距离
     public float attackMaxRange = 40f;          //攻击最大距离
+    public float attackAngle = 45f;             //同检测lookAngle
+    public int attackAccurate = 1;              //同检测lookAccurate
     public float attackRate = 1f;               //攻击周期
     public float attackForce = 15f;             //攻击发射力度
     public int attackDamage = 50;               //攻击伤害
