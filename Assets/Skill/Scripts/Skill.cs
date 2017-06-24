56fee433edc274e6ab5928e023ebebb7e2ece48e
ChiLi_Lin <904981663@163.com>
2017-06-24 11:34:26 +0800

    Add Aim UI And Refactor Skill

--- Assets/Skill/Scripts/Skill.cs
+++ Assets/Skill/Scripts/Skill.cs
@@ -6,6 +6,8 @@ public abstract class Skill : MonoBehaviour
 {
     public float coolDownTime = 1f;                 // 冷却时间
     public Slider coolDownSlider;                   // 冷却条
+    public Camera mainCamera;                       // 相机，用于获取鼠标点击位置
+    public Image aimImage;                          // 瞄准图片
 
     public Color normalColor = Color.white;         // 默认颜色
     public Color hightLightColor = Color.yellow;    // 高亮颜色
@@ -14,6 +16,7 @@ public abstract class Skill : MonoBehaviour
 
     protected Image buttonImage;                    // 按钮图片
     protected float remainReleaseTime = 0f;         // 距离下一次可以释放技能的时间，为0就是可以释放技能
+    protected bool isReady = false;                 // 准备释放技能（第一次点击）
 
     /// <summary>
     /// 初始化滑动条最大值
@@ -33,36 +36,11 @@ public abstract class Skill : MonoBehaviour
     }
 
     /// <summary>
-    /// 点击技能时响应
-    /// </summary>
-    virtual public void OnClicked()
-    {
-        if (CanRelease())
-            Release();
-    }
-
-    /// <summary>
-    /// 鼠标移到按钮时响应
-    /// </summary>
-    virtual public void OnEnter()
-    {
-        buttonImage.color = hightLightColor;
-    }
-
-    /// <summary>
-    /// 鼠标离开按钮是响应
-    /// </summary>
-    virtual public void OnExit()
-    {
-        buttonImage.color = normalColor;
-    }
-
-    /// <summary>
     /// 更新冷却时间，通过Time.deltaTime改变。同时改变按钮颜色和滑动条长度
     /// </summary>
-    virtual public void UpdateCoolDown()
+    private void UpdateCoolDown()
     {
-        coolDownSlider.value = Mathf.Lerp(coolDownSlider.maxValue, coolDownSlider.minValue, remainReleaseTime /(coolDownSlider.maxValue - coolDownSlider.minValue));
+        coolDownSlider.value = Mathf.Lerp(coolDownSlider.maxValue, coolDownSlider.minValue, remainReleaseTime / (coolDownSlider.maxValue - coolDownSlider.minValue));
         if (remainReleaseTime == 0)
             return;
         if (remainReleaseTime < 0)
@@ -75,13 +53,55 @@ public abstract class Skill : MonoBehaviour
         buttonImage.color = disableColor;
     }
 
+    #region 鼠标点击、进入、离开
+
+    /// <summary>
+    /// 点击技能时响应
+    /// </summary>
+    public void OnClicked()
+    {
+        isReady = !isReady;
+        if (isReady)
+        {
+            aimImage.gameObject.SetActive(true);
+            buttonImage.color = pressedColor;
+        }
+        else
+        {
+            aimImage.gameObject.SetActive(false);
+            buttonImage.color = normalColor;
+        }
+    }
+
+    /// <summary>
+    /// 鼠标移到按钮时响应
+    /// </summary>
+    public void OnEnter()
+    {
+        if (isReady)
+            return;
+        buttonImage.color = hightLightColor;
+    }
+
+    /// <summary>
+    /// 鼠标离开按钮时响应
+    /// </summary>
+    public void OnExit()
+    {
+        if (isReady)
+            return;
+        buttonImage.color = normalColor;
+    }
+
+    #endregion
+
     /// <summary>
     /// 是否可释放技能
     /// </summary>
     /// <returns>返回True，可以释放技能</returns>
     virtual public bool CanRelease()
     {
-        return remainReleaseTime <= 0f;
+        return remainReleaseTime <= 0f && isReady;
     }
 
     /// <summary>
@@ -90,6 +110,7 @@ public abstract class Skill : MonoBehaviour
     virtual public void Release()
     {
         remainReleaseTime = coolDownSlider.maxValue;
+        isReady = false;
         SkillEffect();
     }
 
--- Assets/Skill/Scripts/ThunderSkill.cs
+++ Assets/Skill/Scripts/ThunderSkill.cs
@@ -4,8 +4,44 @@ using UnityEngine.EventSystems;
 
 public class ThunderSkill : Skill
 {
+    private Vector3 inputHitPos;
+
+    private void Update()
+    {
+        if (CanRelease())
+        {
+            aimImage.transform.position = Input.mousePosition;
+            //RaycastObject(Input.mousePosition);
+        }
+    }
+
+
+
+    /// <summary>
+    /// 技能效果
+    /// </summary>
     public override void SkillEffect()
     {
         Debug.Log("Fuck Everything");
+        
+    }
+
+
+    private GameObject RaycastObject(Vector2 screenPos)
+    {
+        RaycastHit info;
+        if (Physics.Raycast(mainCamera.ScreenPointToRay(screenPos), out info, 200))
+        {
+            inputHitPos = info.point;
+            //Debug.Log(inputHitPos + "  " + info.collider.gameObject.name);
+            return info.collider.gameObject;
+        }
+        return null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(inputHitPos, 1.5f); 
     }
 }
