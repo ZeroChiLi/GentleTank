da458d0e98402c44233794fec61718679ddada3d
ChiLi_Lin <904981663@163.com>
2017-07-02 21:29:45 +0800

    Add Flag Perfabs

--- Assets/Main Assets/Scripts/UI/PlayerInfoUI.cs
+++ Assets/Main Assets/Scripts/UI/PlayerInfoUI.cs
@@ -2,10 +2,18 @@
 
 public class PlayerInfoUI : MonoBehaviour 
 {
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+        
     private void Update()
     {
+        //Debug.Log("Position : " + mainCamera.transform.position + " --- eulerAngles : " + mainCamera.transform.rotation.eulerAngles);
         //将UI对着相机设备
-        GetComponent<RectTransform>().rotation = Quaternion.LookRotation(-1 * GameObject.FindGameObjectWithTag("MainCamera").transform.position);
+        GetComponent<RectTransform>().rotation = Quaternion.LookRotation(-1 * mainCamera.transform.position)/* * Quaternion.Euler(mainCamera.transform.rotation.eulerAngles - new Vector3(0,40,0))*/;
     }
 
     // 设置显示到镜头玩家名字
