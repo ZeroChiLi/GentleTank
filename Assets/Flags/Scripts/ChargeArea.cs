c29daef5d8e5339d4c78b77e9f1e0e56520b9bd7
ChiLi_Lin <904981663@163.com>
2017-07-07 00:06:40 +0800

    Add Basic Function To ChargeArea

--- Assets/Flags/Scripts/ChargeArea.cs
+++ Assets/Flags/Scripts/ChargeArea.cs
@@ -12,24 +12,23 @@ public class ChargeArea : MonoBehaviour
     public float maxValue = 100f;           // 滑动条最大值
     public float chargeValue = 5f;          // 每秒变化量
 
-    private List<TankInformation> playerInfoList;    // 在区域内的所有玩家及其对应信息
-    private float currentValue;             // 当前值
-    private List<GameObject> majorPlayers;  // 占主力的玩家们
-    private Dictionary<int,int> majorTeam;  // 区域内每个团队所占人数（团队ID：团队人数），没队的为1
-    private float updateTime = 0.1f;        // 更新时间
-    private float elapsedTime = 0f;         // 计时器
+    private List<TankInformation> playerInfoList;   // 在区域内的所有玩家及其对应信息
     private List<TankInformation> inactivePlayers;  // 失活的坦克
+    private float currentValue;                     // 当前值
+    private float updateTime = 0.1f;                // 更新时间
+    private float elapsedTime = 0f;                 // 计时器
+    private bool occupied = false;                  // 旗帜区域是否已经被占有
+    private bool stalemate = false;                 // 是否保持僵局（区域内不止一个队伍）
+    private List<TankInformation> occupyPlayerList; // 占有旗帜队伍的所有圈内成员
 
     /// <summary>
     /// 设置滑动条大小、碰撞体大小
     /// </summary>
     private void Awake()
     {
-        areaCanvas.GetComponent<RectTransform>().sizeDelta = Vector2.one * radius * 2;
         GetComponent<SphereCollider>().radius = radius;
+        areaCanvas.GetComponent<RectTransform>().sizeDelta = Vector2.one * radius * 2;
         playerInfoList = new List<TankInformation>();
-        majorPlayers = new List<GameObject>();
-        majorTeam = new Dictionary<int, int>();
         inactivePlayers = new List<TankInformation>();
     }
 
@@ -66,26 +65,7 @@ public class ChargeArea : MonoBehaviour
         if (!ContainAnyPlayer())
             return;
         CleanInactivePlayer();
-    }
-
-    private void UpdateMembers()
-    {
-        
-    }
-
-    /// <summary>
-    /// 移除所有在圈内且已经死掉的玩家
-    /// </summary>
-    private void CleanInactivePlayer()
-    {
-        inactivePlayers.Clear();
-
-        for (int i = 0; i < playerInfoList.Count; i++)
-            if (!playerInfoList[i].gameObject.activeSelf)
-                inactivePlayers.Add(playerInfoList[i]);
-
-        for (int i = 0; i < inactivePlayers.Count; i++)
-            playerInfoList.Remove(inactivePlayers[i]);
+        UpdateCurrentValue();
     }
 
     /// <summary>
@@ -97,6 +77,65 @@ public class ChargeArea : MonoBehaviour
         return playerInfoList.Count != 0;
     }
 
+    /// <summary>
+    /// 移除所有在圈内且已经死掉的玩家
+    /// </summary>
+    private void CleanInactivePlayer()
+    {
+        inactivePlayers.Clear();
+
+        for (int i = 0; i < playerInfoList.Count; i++)              //先获取所有失效玩家
+            if (!playerInfoList[i].gameObject.activeSelf)
+                inactivePlayers.Add(playerInfoList[i]);
+
+        for (int i = 0; i < inactivePlayers.Count; i++)             //再删除所有失效玩家
+            playerInfoList.Remove(inactivePlayers[i]);
+    }
+
+    /// <summary>
+    /// 更新当前数值
+    /// </summary>
+    private void UpdateCurrentValue()
+    {
+
+    }
+
+    /// <summary>
+    /// 更新占有权值，通过rank来控制更新倍数
+    /// </summary>
+    /// <param name="rank">更新倍数</param>
+    private void UpdateOccupation(float rank)
+    {
+
+    }
+
+    /// <summary>
+    /// 是否只有一个队伍（或个人）在区域内
+    /// </summary>
+    /// <returns>是否只有一个队伍（或个人）在区域内</returns>
+    private bool OnlyOneTeamHere()
+    {
+
+        return true;
+    }
+
+    /// <summary>
+    /// 保持僵局，按人数分配扇区颜色
+    /// </summary>
+    private void KeepStalemate()
+    {
+
+    }
+
+    /// <summary>
+    /// 占有属于玩家
+    /// </summary>
+    /// <param name="player">玩家信息</param>
+    /// <returns>是否被该玩家占有</returns>
+    private bool OccupiedByPlayer(TankInformation player)
+    {
+        return true;
+    }
 }
 
 
