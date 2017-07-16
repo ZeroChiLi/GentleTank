f71a4869138cb95dd10de9b0a2d8725c8fe715fa
ChiLi_Lin <904981663@163.com>
2017-07-16 14:30:15 +0800

    Add Aim Model

---
+++ Assets/Skill/Editor/AimModelEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AimMode))]
+[CanEditMultipleObjects]
+public class AimModelEditor : Editor 
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.TagField("Tag","Untagged");
+    }
+}
---
+++ Assets/Skill/Scripts/Aim/AimModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(menuName = "Aim Model/Default Aim Model")]
+public class AimMode : ScriptableObject
+{
+    public Dictionary<string, Color> tagColorDic = new Dictionary<string, Color>();
+
+    
+}
