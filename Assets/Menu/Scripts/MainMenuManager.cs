c1a95c0f198354bb83b32a60d80456afcfcce2f6
ChiLi_Lin <904981663@163.com>
2017-07-24 18:23:12 +0800

    Add Main Menu Scene

---
+++ Assets/Menu/Scripts/MainMenuManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour 
+{
+    /// <summary>
+    /// 加载单人模式场景
+    /// </summary>
+    public void LoadSoloScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
