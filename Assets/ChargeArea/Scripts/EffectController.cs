9424ff0ac55ad9d9683d320ee4d19ca55d23eab2
ChiLi_Lin <904981663@163.com>
2017-08-09 22:58:33 +0800

    Rename Flag To ChargeArea

---
+++ Assets/ChargeArea/Scripts/EffectController.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+
+namespace Item.ChargeArea
+{
+    // 特效状态，吸收、混乱、释放
+    public enum EffectState
+    {
+        None, Absorb, Chaos, Release, Completed, Crack
+    }
+
+    public class EffectController : MonoBehaviour
+    {
+        public ObjectPool AbsorbEffectPool;             // 吸收效果池
+        public ObjectPool ChaosEffectPool;              // 混乱效果池
+        public ObjectPool ReleaseEffectPool;            // 释放效果池
+        public ObjectPool CompletedEffectPool;          // 完成效果池
+        public ObjectPool CrackEffectPool;              // 默认效果池
+
+        public EffectState CurrentState { get { return currentState; } }    // 获取当前特效状态
+
+        private EffectState currentState = EffectState.None;    // 当前特效状态
+        private GameObject currentEffect;                       // 当前特性对象
+        private ParticleSystem currentParticle;                 // 当前特效的粒子系统
+        private ParticleSystem.MainModule currentParticleMain;  // 当前特效的粒子的主模型
+        private ParticleSystem.ShapeModule currentParticleShape;// 当前特效粒子的发射形状
+        private float radius;                                   // 特效发射半径
+        private Transform currentEffectTransform;               // 特效位置
+
+        /// <summary>
+        /// 设置粒子发射的半径
+        /// </summary>
+        /// <param name="radius">粒子发射的半径</param>
+        public void SetParticleShapeRaidus(float radius)
+        {
+            this.radius = radius;
+        }
+
+        /// <summary>
+        /// 关闭特效
+        /// </summary>
+        public void CloseEffect()
+        {
+            if (!EffectActive())
+                return;
+            currentEffect.SetActive(false);
+            currentState = EffectState.None;
+            currentEffect = null;
+        }
+
+        /// <summary>
+        /// 设置特效
+        /// </summary>
+        /// <param name="effect">特效的状态</param>
+        /// <param name="transform">特效的位置</param>
+        /// <param name="color">粒子特效颜色</param>
+        public void SetEffect(EffectState effect, Transform transform, Color color)
+        {
+            if (effect == currentState)
+                return;
+            CloseEffect();
+            currentState = effect;
+            currentEffect = GetEffectObject(effect, transform);             // 先获取特效
+
+            currentParticle = currentEffect.GetComponent<ParticleSystem>(); // 获取特效的粒子
+            currentParticleMain = currentParticle.main;
+            currentParticleMain.startColor = color;                         // 设置粒子颜色
+
+            currentParticleShape = currentParticle.shape;
+            currentParticleShape.radius = radius;                           // 设置粒子发射半径
+
+            currentEffect.SetActive(true);                                  // 最后再激活特效
+        }
+
+        /// <summary>
+        /// 获取特效，失败返回null
+        /// </summary>
+        /// <param name="effect">特效状态</param>
+        /// <param name="transform">特效的位置</param>
+        /// <returns>特效对象</returns>
+        public GameObject GetEffectObject(EffectState effect, Transform transform)
+        {
+            GameObject effectObject = null;
+            switch (effect)
+            {
+                case EffectState.Absorb:
+                    effectObject = AbsorbEffectPool.GetNextObject(false, transform);
+                    break;
+                case EffectState.Chaos:
+                    effectObject = ChaosEffectPool.GetNextObject(false, transform);
+                    break;
+                case EffectState.Release:
+                    effectObject = ReleaseEffectPool.GetNextObject(false, transform);
+                    break;
+                case EffectState.Completed:
+                    effectObject = CompletedEffectPool.GetNextObject(false, transform);
+                    break;
+                case EffectState.Crack:
+                    effectObject = CrackEffectPool.GetNextObject(false, transform);
+                    break;
+            }
+            if (effectObject != null)
+                effectObject.transform.Rotate(-90, 0, 0);
+            return effectObject;
+        }
+
+        /// <summary>
+        /// 特效是否正确有效
+        /// </summary>
+        /// <returns>是否正确有效</returns>
+        public bool EffectActive()
+        {
+            if (currentState == EffectState.None || currentEffect == null)
+                return false;
+            return true;
+        }
+
+        public void SetParticleColor(Color color)
+        {
+            if (EffectActive())
+                currentParticleMain.startColor = color;
+        }
+
+    }
+}
---
+++ Assets/ChargeArea/Scripts/FlagManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Item.ChargeArea
+{
+    public class FlagManager : MonoBehaviour
+    {
+        public GameObject flagPole;             // 旗杆，通过旋转旗杆达到旗帜面对镜头
+        public MeshRenderer flagMeshRender;     // 旗帜的渲染网格
+
+        private Material flagMaterial;          // 旗帜的材质，拿来改颜色
+
+        /// <summary>
+        /// 旋转旗杆，使旗帜面对镜头
+        /// </summary>
+        private void Awake()
+        {
+            flagMaterial = flagMeshRender.material;
+            flagPole.transform.rotation = Quaternion.Euler(GameObject.FindGameObjectWithTag("MainCamera").transform.rotation.eulerAngles);
+            flagPole.transform.localEulerAngles = new Vector3(0f, flagPole.transform.localEulerAngles.y, flagPole.transform.localEulerAngles.z);
+        }
+
+        /// <summary>
+        /// 设置旗帜颜色
+        /// </summary>
+        /// <param name="color">旗帜颜色</param>
+        public void SetFlagColor(Color color)
+        {
+            flagMaterial.color = color;
+        }
+
+    }
+}
