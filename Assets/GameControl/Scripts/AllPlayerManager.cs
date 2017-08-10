46b78e2447f8b14f23768078b83e8ded8618e30e
ChiLi_Lin <904981663@163.com>
2017-08-11 01:43:30 +0800

    Add 'PlayerManager' , 'AllPlayerManager' , 'PlayerInformation'

---
+++ Assets/GameControl/Scripts/AllPlayerManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "All Player Manager")]
+public class AllPlayerManager : ScriptableObject
+{
+    static private AllPlayerManager instance;                           // 所有玩家列表单例
+    static public AllPlayerManager Instance { get { return instance = instance ?? new AllPlayerManager(); } }
+
+    public List<PlayerInformation> playerInformationList;               // 玩家信息列表（用于外部配置创建游戏对象）
+
+    private List<PlayerManager> playerManagerList;                      // 玩家列表（用于作为组件添加到游戏对象）
+
+    public int Count { get { return playerManagerList.Count; } }        // 玩家数量
+
+    public PlayerManager this[int index] { get { return playerManagerList[index]; } }
+
+    /// <summary>
+    /// 创建玩家对象们（playerManagerList）
+    /// </summary>
+    public void CreatePlayerGameObjects()
+    {
+        playerManagerList = new List<PlayerManager>();
+        for (int i = 0; i < playerInformationList.Count; i++)
+            playerManagerList.Add(playerInformationList[i].CreateGameObjectWithPlayerManager());
+    }
+
+    /// <summary>
+    /// 是否包含玩家
+    /// </summary>
+    /// <param name="player">玩家</param>
+    /// <returns>是否存在在列表中</returns>
+    public bool Contain(PlayerManager player)
+    {
+        return playerManagerList.Contains(player);
+    }
+
+    /// <summary>
+    /// 添加玩家
+    /// </summary>
+    /// <param name="player">新玩家</param>
+    public void Add(PlayerManager player)
+    {
+        if (Contain(player))
+            return;
+        playerManagerList.Add(player);
+    }
+
+    /// <summary>
+    /// 移除玩家
+    /// </summary>
+    /// <param name="player">要移除的玩家</param>
+    public void Remove(PlayerManager player)
+    {
+        if (!Contain(player))
+            return;
+        playerManagerList.Remove(player);
+    }
+
+    /// <summary>
+    /// 判断两人是否是队友
+    /// </summary>
+    /// <param name="player1">玩家1</param>
+    /// <param name="player2">玩家2</param>
+    /// <returns></returns>
+    public bool IsTeammate(PlayerManager player1,PlayerManager player2)
+    {
+        if (!Contain(player1) || !Contain(player2))
+            return false;
+        return player1.IsTeammate(player2);
+    }
+
+    /// <summary>
+    /// 获取第一个归自己控制的玩家
+    /// </summary>
+    /// <returns>自己控制的玩家</returns>
+    public PlayerManager GetMyPlayer()
+    {
+        for (int i = 0; i < playerManagerList.Count; i++)
+            if (playerManagerList[i].IsMine)
+                return playerManagerList[i];
+        return null;
+    }
+}
---
+++ Assets/GameControl/Scripts/PlayerManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class PlayerInformation
+{
+    public int id;                          // 玩家ID
+    public string name;                     // 玩家名称
+    public bool isMine;                     // 玩家是否是自己控制的
+    public bool isActive;                   // 玩家是否激活
+    public bool isAI;                       // 玩家是否是AI
+    public GameObject perfab;               // 玩家预设
+    public Color representColor;            // 玩家代表颜色
+    public TeamManager team;                // 玩家所在的团队，没有则为null
+
+    /// <summary>
+    /// 创建游戏对象实例，并把该玩家信息作为组件（PlayerManager）添加进去
+    /// </summary>
+    /// <returns>返回玩家信息管理</returns>
+    public PlayerManager CreateGameObjectWithPlayerManager()
+    {
+        GameObject gameObject = Object.Instantiate(perfab);
+        PlayerManager playerManager = gameObject.GetComponent<PlayerManager>();
+        playerManager.SetInformation(this);
+        return playerManager;
+    }
+}
+
+public class PlayerManager : MonoBehaviour
+{
+    private PlayerInformation information;      // 玩家信息
+
+    public int Id { get { return information.id; } }
+    public string Name { get { return information.name; } }
+    public bool IsMine { get { return information.isMine; } }
+    public bool Active { get { return information.isActive; } }
+    public bool IsAI { get { return information.isAI; } }
+    public Color RepresentColor { get { return information.representColor; } }
+    public TeamManager Team { get { return information.team; } }
+
+    /// <summary>
+    /// 设置玩家信息
+    /// </summary>
+    /// <param name="playerInfo">玩家信息</param>
+    public void SetInformation(PlayerInformation playerInfo)
+    {
+        information = playerInfo;
+    }
+
+    /// <summary>
+    /// 判断传入玩家是否是队友
+    /// </summary>
+    /// <param name="player">另一个玩家</param>
+    /// <returns>是否是队友</returns>
+    public bool IsTeammate(PlayerManager player)
+    {
+        if (Team == null || player.Team == null)
+            return false;
+
+        if (Team == player.Team)
+            return true;
+        else
+            return false;
+    }
+
+}
