0edcd51866926c10cd79faeb16c51184735e2d57
ChiLi_Lin <904981663@163.com>
2017-08-31 00:14:15 +0800

    Add TankModules

---
+++ Assets/Items/Tank/Scripts/TankAssemble.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TankAssemble : ScriptableObject 
+{
+    public string tankName;
+    public TankModule bodyModule;
+    public TankModule turretModule;
+    public TankModule wheelLeftModule;
+    public TankModule wheelRightModule;
+
+    private GameObject body;
+    private GameObject turret;
+    private GameObject wheelLeft;
+    private GameObject wheelRight;
+
+    public GameObject Create()
+    {
+        if (!Check())
+            return null;
+
+        //PrefabUtility.CreatePrefab(path, obj);
+        GameObject newTank = new GameObject(tankName);
+        body = Instantiate(bodyModule.prefab, newTank.transform);
+        turret = Instantiate(turretModule.prefab, newTank.transform);
+        wheelLeft = Instantiate(wheelLeftModule.prefab, newTank.transform);
+        wheelRight = Instantiate(wheelRightModule.prefab, newTank.transform);
+
+        turret.transform.localPosition = bodyModule[ModuleType.Turret].anchor - turretModule[ModuleType.Body].anchor;
+        wheelLeft.transform.localPosition = bodyModule[ModuleType.WheelLeft].anchor - wheelLeftModule[ModuleType.Body].anchor;
+        wheelRight.transform.localPosition = bodyModule[ModuleType.WheelRight].anchor - wheelRightModule[ModuleType.Body].anchor;
+
+
+
+
+        return newTank;
+    }
+
+    public bool Check()
+    {
+        return (wheelLeftModule != null && wheelLeftModule.type == ModuleType.WheelLeft
+            && wheelRightModule != null && wheelRightModule.type == ModuleType.WheelRight
+            && bodyModule != null && bodyModule.type == ModuleType.Body
+            && turretModule != null && turretModule.type == ModuleType.Turret);
+    }
+
+}
---
+++ Assets/Items/Tank/Scripts/TankModule.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum ModuleType
+{
+    None, Turret, Body, WheelLeft,WheelRight
+}
+
+[System.Serializable]
+public class ConnectAnchor
+{
+    public ModuleType type;
+    public Vector3 anchor;
+    public Vector3 rotation;
+}
+
+[CreateAssetMenu(menuName = "TankModule")]
+public class TankModule : ScriptableObject 
+{
+    public GameObject prefab;
+    public ModuleType type;
+    public List<ConnectAnchor> connectAnchor = new List<ConnectAnchor>();
+
+    public ConnectAnchor this[ModuleType type]
+    {
+        get
+        {
+            for (int i = 0; i < connectAnchor.Count; i++)
+                if (connectAnchor[i].type == type)
+                    return connectAnchor[i];
+            return null;
+        }
+    }
+}
