f5e923865c2a497ce84eaa2cbe9968724e215d3c
ChiLi_Lin <904981663@163.com>
2017-09-16 14:21:58 +0800

    Update TankModule

--- Assets/Items/Tank/Editor/TankModuleEditor.cs
+++ Assets/Items/Tank/Editor/TankModuleEditor.cs
@@ -18,10 +18,7 @@ public class TankModuleEditor : Editor
     {
         base.OnInspectorGUI();
         if (tankModule.prefab != null && targetPrefab != tankModule.prefab)
-        {
             targetPrefab = tankModule.prefab;
-            //SetDefaultValue();
-        }
         SetDefaultValueButton();
     }
 
@@ -30,8 +27,30 @@ public class TankModuleEditor : Editor
     /// </summary>
     virtual public void SetDefaultValueButton()
     {
-        if (!GUILayout.Button("Set Default Anchor Value") || tankModule.prefab == null)
+        if (tankModule.prefab == null)
             return;
+        if (GUILayout.Button("Set Default By MeshFilter"))
+            SetDefaultValueByMeshFilter();
+        if (GUILayout.Button("Set Default By BoxCollider"))
+            SetDefaultValueByBoxCollider();
+    }
+
+    /// <summary>
+    /// 拖过网格过滤器设置默认值
+    /// </summary>
+    public void SetDefaultValueByMeshFilter()
+    {
+        moduleBounds = tankModule.prefab.GetComponent<MeshFilter>().sharedMesh.bounds;
+        SetDefaultValue();
+    }
+    
+    /// <summary>
+    /// 通过盒子碰撞体设置默认值
+    /// </summary>
+    public void SetDefaultValueByBoxCollider()
+    {
+        BoxCollider collider = tankModule.prefab.GetComponent<BoxCollider>();
+        moduleBounds = new Bounds(collider.center,collider.size);
         SetDefaultValue();
     }
 
@@ -40,9 +59,8 @@ public class TankModuleEditor : Editor
     /// </summary>
     virtual public void SetDefaultValue()
     {
-        moduleBounds = tankModule.prefab.GetComponent<MeshFilter>().sharedMesh.bounds;
         tankModule.anchors.center = GameMathf.Round(moduleBounds.center);
-        tankModule.anchors.forward = GameMathf.Round(moduleBounds.center + new Vector3(0,0,moduleBounds.extents.z));
+        tankModule.anchors.forward = GameMathf.Round(moduleBounds.center + new Vector3(0, 0, moduleBounds.extents.z));
         tankModule.anchors.back = GameMathf.Round(moduleBounds.center + new Vector3(0, 0, -moduleBounds.extents.z));
         tankModule.anchors.left = GameMathf.Round(moduleBounds.center + new Vector3(-moduleBounds.extents.x, 0, 0));
         tankModule.anchors.right = GameMathf.Round(moduleBounds.center + new Vector3(moduleBounds.extents.x, 0, 0));
