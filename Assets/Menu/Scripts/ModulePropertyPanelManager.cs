5a85b79189a37d02b71a9296352fc1b21df1b6dd
ChiLi_Lin <904981663@163.com>
2017-09-19 18:01:23 +0800

    Add Adaptation To AmmoModulePreview

--- Assets/Menu/Scripts/ModulePropertyPanelManager.cs
+++ Assets/Menu/Scripts/ModulePropertyPanelManager.cs
@@ -8,17 +8,24 @@ public class ModulePropertyPanelManager : MonoBehaviour
     static private ModulePropertyPanelManager instance;
     static public ModulePropertyPanelManager Instance { get { return instance; } }
 
-    public RectTransform rectTransform;
-    public Text titleText;
-    public Text propertyText;
-    public List<string> propertyStrList;
+    public RectTransform rectTransform;         // 自身面板的转换
+    public Text titleText;                      // 标题文本
+    public Text propertyText;                   // 属性列表文本
+    public bool autoAdapt = true;               // 是否自适应
+    public List<string> propertyStrList;        // 属性字符串列表
+    public CanvasScaler scaler;                 // 所在面板缩放大小
 
     private StringBuilder str;
+    private Vector2 scalerFator;
+    private Vector2 fixedPos;
+    private Vector2 offset;
 
     private void Awake()
     {
         instance = this;
         gameObject.SetActive(false);
+        if (scaler == null)
+            scaler = GetComponentInParent<CanvasScaler>();
     }
 
     /// <summary>
@@ -29,6 +36,7 @@ public class ModulePropertyPanelManager : MonoBehaviour
     /// <param name="properties">信息字符串数组</param>
     public void Show(Vector3 pos, string title,params string[] properties)
     {
+        SetDefaultAdaptation();
         gameObject.SetActive(true);
         rectTransform.position = pos;
         titleText.text = title;
@@ -40,5 +48,33 @@ public class ModulePropertyPanelManager : MonoBehaviour
         }
         propertyText.text = str.ToString();
         rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, titleText.preferredHeight + propertyText.preferredHeight);
+        if (autoAdapt)
+            AdaptToScreen();
+    }
+
+    /// <summary>
+    /// 设置默认适配
+    /// </summary>
+    public void SetDefaultAdaptation()
+    {
+        rectTransform.anchorMin = new Vector2(0, 1);
+        rectTransform.anchorMax = new Vector2(0, 1);
+        rectTransform.pivot = new Vector2(0, 1);
+    }
+
+    /// <summary>
+    /// 适应到屏幕
+    /// </summary>
+    public void AdaptToScreen()
+    {
+        scalerFator = new Vector2(Screen.width / scaler.referenceResolution.x, Screen.height / scaler.referenceResolution.y);
+        fixedPos = rectTransform.position;
+        offset.x = rectTransform.position.x + rectTransform.sizeDelta.x * scalerFator.x - Screen.width;
+        offset.y = rectTransform.position.y - rectTransform.sizeDelta.y * scalerFator.y;
+        if (offset.x > 0)
+            fixedPos.x -= offset.x;
+        if (offset.y < 0)
+            fixedPos.y -= offset.y;
+        rectTransform.position = fixedPos;
     }
 }
