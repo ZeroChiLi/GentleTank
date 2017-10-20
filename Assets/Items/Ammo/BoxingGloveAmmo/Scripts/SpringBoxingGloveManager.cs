9bf36dc81e62f746e67e5b85aae82087dfb142bf
ChiLi_Lin <904981663@163.com>
2017-10-20 15:18:44 +0800

    Refactor SpringBoxingGlove

---
+++ Assets/Items/Ammo/BoxingGloveAmmo/Scripts/SpringBoxingGloveManager.cs
@@ -0,0 +1,37 @@
+﻿using Item.Ammo;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SpringBoxingGloveManager : MonoBehaviour
+{
+    public BoxCollider glove;                       // 拳套碰撞体
+    public BoxingAmmo ammo;                         // 拳套弹药
+    public Transform spring;                        // 弹簧
+    public float scaleFactor = 200f;                // 弹簧缩放随锚点位置变化的缩放因子
+    public float minDistance = 0f;                  // 最小距离
+    public float maxDistance = 5f;                  // 最大距离
+    [Range(0, 1)]
+    public float fillAmount = 0.5f;                 // 距离插值
+
+    public float CurrentDistance { get { return currentDistance; } }
+    private float currentDistance;                  // 当前距离
+
+    /// <summary>
+    /// 更新当前距离，弹簧缩放，锚点位置
+    /// </summary>
+    private void LateUpdate()
+    {
+        currentDistance = Mathf.Lerp(minDistance, maxDistance, fillAmount);
+        UpdateAnchorPosAndSpringScale();
+    }
+
+    /// <summary>
+    /// 更新锚点位置和弹簧缩放
+    /// </summary>
+    private void UpdateAnchorPosAndSpringScale()
+    {
+        glove.transform.localPosition = new Vector3(0, 0, currentDistance);
+        spring.localScale = new Vector3(0, 0, scaleFactor * glove.transform.localPosition.z);
+    }
+
+}
--- Assets/Items/Tank/Scripts/TankAttackBoxing.cs
+++ Assets/Items/Tank/Scripts/TankAttackBoxing.cs
@@ -6,9 +6,7 @@ namespace Item.Tank
 {
     public class TankAttackBoxing : TankAttack
     {
-        public SpringManager springManager;     // 弹簧控制
-        public BoxingAmmo boxingGlove;          // 弹簧拳组件
-        public Collider boxingCollider;         // 拳套
+        public SpringBoxingGloveManager springBoxingGlove;     // 弹簧控制
         public AnimationCurve launchDistance = AnimationCurve.Linear(0, 0, 0.4f, 1);    // 发射距离比例,来回对称
         public float launchTotalTime = 0.8f;    // 总共发射来回时间
 
@@ -17,13 +15,13 @@ namespace Item.Tank
         protected new void OnEnable()
         {
             base.OnEnable();
-            springManager.fillAmount = 0;
+            springBoxingGlove.fillAmount = 0;
         }
 
         protected new void OnDisable()
         {
             base.OnDisable();
-            springManager.fillAmount = 0;
+            springBoxingGlove.fillAmount = 0;
         }
 
         /// <summary>
@@ -45,9 +43,9 @@ namespace Item.Tank
         /// <param name="coolDownTime">发射后冷却时间</param>
         private void Launch(float launchForce, float fireDamage, float coolDownTime)
         {
-            springManager.maxDistance = launchForce / 10f;
-            boxingGlove.damage = fireDamage;
-            boxingCollider.enabled = true;
+            springBoxingGlove.maxDistance = launchForce / 10f;
+            springBoxingGlove.ammo.damage = fireDamage;
+            springBoxingGlove.glove.enabled = true;
 
             StartCoroutine(LaunchBoxingGlove());
 
@@ -61,33 +59,33 @@ namespace Item.Tank
         private IEnumerator LaunchBoxingGlove()
         {
             launchElapsed = 0f;
-            boxingGlove.needTurnBack = false;
+            springBoxingGlove.ammo.needTurnBack = false;
             while (launchElapsed < launchTotalTime && launchElapsed >= 0f)
             {
-                if (!boxingGlove.needTurnBack && launchElapsed < launchTotalTime / 2f)
+                if (!springBoxingGlove.ammo.needTurnBack && launchElapsed < launchTotalTime / 2f)
                 {
-                    springManager.fillAmount = launchDistance.Evaluate(launchElapsed);
+                    springBoxingGlove.fillAmount = launchDistance.Evaluate(launchElapsed);
                     launchElapsed += Time.deltaTime;
                 }
                 else
                 {
-                    if (boxingGlove.needTurnBack)
+                    if (springBoxingGlove.ammo.needTurnBack)
                     {
                         launchElapsed -= Time.deltaTime;
-                        springManager.fillAmount = launchDistance.Evaluate(launchElapsed);
+                        springBoxingGlove.fillAmount = launchDistance.Evaluate(launchElapsed);
                     }
                     else
                     {
                         launchElapsed += Time.deltaTime;
-                        springManager.fillAmount = launchDistance.Evaluate(launchTotalTime - launchElapsed);
+                        springBoxingGlove.fillAmount = launchDistance.Evaluate(launchTotalTime - launchElapsed);
                     }
-                    boxingCollider.enabled = false;
+                    springBoxingGlove.glove.enabled = false;
                 }
 
                 yield return null;
             }
-            springManager.fillAmount = 0f;
-            boxingCollider.enabled = false;
+            springBoxingGlove.fillAmount = 0f;
+            springBoxingGlove.glove.enabled = false;
         }
     }
 }
