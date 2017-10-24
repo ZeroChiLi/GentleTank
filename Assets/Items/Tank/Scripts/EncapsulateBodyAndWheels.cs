486ff6cadb522525a0e0df4cb94747fb548ec414
ChiLi_Lin <904981663@163.com>
2017-10-24 17:34:52 +0800

    Encapsulate TankBody And TankWheels Collider.

---
+++ Assets/Items/Tank/Scripts/EncapsulateBodyAndWheels.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EncapsulateBodyAndWheels : MonoBehaviour
+{
+    public BoxCollider targetCollider;
+    public List<BoxCollider> targets = new List<BoxCollider>();
+
+    private void Update()
+    {
+        if (targetCollider == null || targets == null || targets.Count <= 0)
+            return;
+        Encapsulate(ref targetCollider);
+    }
+
+    public void Encapsulate(ref BoxCollider collider)
+    {
+        Bounds bounds = new Bounds();
+        bounds = targets[0].bounds;
+        for (int i = 0; i < targets.Count; i++)
+            if (targets[i] != null)
+                bounds.Encapsulate(targets[i].bounds);
+        collider.center = bounds.center;
+        collider.size = bounds.size;
+    }
+
+
+}
