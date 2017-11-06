2e6520b95250506154de7bdc942002ad0e4086f6
ChiLi_Lin <904981663@163.com>
2017-11-06 16:27:40 +0800

    Add SpriteProcessor.

---
+++ Assets/Common/Editor/SpriteProcessor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class SpriteProcessor : AssetPostprocessor
+{
+    private void OnPostprocessTexture(Texture2D texture)
+    {
+        if (assetPath.ToLower().IndexOf("/sprites/") != -1)
+        {
+            TextureImporter textureImporter = (TextureImporter)assetImporter;
+            textureImporter.textureType = TextureImporterType.Sprite;
+            textureImporter.spriteImportMode = SpriteImportMode.Single;
+            textureImporter.alphaIsTransparency = true;
+            textureImporter.mipmapEnabled = false;
+        }
+    }
+}
