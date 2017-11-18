a337377c1ebde2023b602d42aa004a89517952d1
ChiLi_Lin <904981663@163.com>
2017-11-18 13:50:35 +0800

    Add SkillEventSystem.

---
+++ Assets/GameSystems/Skill/Scripts/SkillEventSystem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SkillEventSystem : MonoBehaviour 
+{
+    public EventSystem eventSystem;
+
+    public CoolDownButton firstClick;
+
+
+}
