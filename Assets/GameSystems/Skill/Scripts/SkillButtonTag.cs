b630247c31316401c98a17c19495dbb263b7b3ef
ChiLi_Lin <904981663@163.com>
2017-11-19 13:53:24 +0800

    Update SkillButton.

---
+++ Assets/GameSystems/Skill/Scripts/SkillButtonTag.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillButtonTag : MonoBehaviour 
+{
+    public CoolDownButton target;
+
+    private void OnEnable()
+    {
+        Button btn = GetComponent<Button>();
+        if (btn != null)
+        {
+            btn.onClick.AddListener(OnSkillButtonClick);
+        }
+    }
+
+    public void OnSkillButtonClick()
+    {
+        SkillEventSystem.Instance.SkillButtonClicked(target);
+    }
+
+}
--- Assets/GameSystems/Skill/Scripts/SkillEventSystem.cs
+++ Assets/GameSystems/Skill/Scripts/SkillEventSystem.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class SkillEventSystem : MonoBehaviour 
+public class SkillEventSystem : MonoBehaviour
 {
+    static public SkillEventSystem Instance { private set; get; }
+
     public EventSystem eventSystem;
 
+    public bool skillButtonClicked;
     public CoolDownButton firstClick;
 
+    private void Awake() { Instance = this; }
+
+    private void OnGUI()
+    {
+        //if ()
+        //{
+
+        //}
+    }
+
+    public void SkillButtonClicked(CoolDownButton target)
+    {
+
+    }
 
 }
