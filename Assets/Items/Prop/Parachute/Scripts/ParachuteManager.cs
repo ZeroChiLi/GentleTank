a3d0f3913708f93b1c0d55f0fe0df8b1c5bc5ae2
ChiLi_Lin <904981663@163.com>
2017-12-21 14:38:10 +0800

    Add Parachute Open/Close Animation.

---
+++ Assets/Items/Prop/Parachute/Scripts/ParachuteManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ParachuteManager : MonoBehaviour
+{
+    public Animator animator;
+
+    public void Open()
+    {
+        animator.SetTrigger("Open");
+    }
+
+    public void Close()
+    {
+        animator.SetTrigger("Close");
+    }
+}
