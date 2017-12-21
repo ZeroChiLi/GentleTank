f76e39ca9040ba5484b99a92f370dcc9142a9b62
ChiLi_Lin <904981663@163.com>
2017-12-21 22:33:06 +0800

    Add BlinkPostEffect To MedicalKit.

---
+++ Assets/Items/Prop/Scripts/BlinkPostEffect.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BlinkPostEffect : PostEffectsBase 
+{
+    public bool blinkAtAwake = true;
+    public float blinkTime = 1f;
+    [Range(0f, 1f)]
+    public float maxColorAlpha = 0.5f;
+    public AnimationCurve blinkCurve;
+    public Color color = Color.green;
+    public MeshFilter[] meshFilters;
+    public bool isOpen;
+    public CountDownTimer timer;
+
+    private void Awake()
+    {
+        timer = new CountDownTimer(blinkTime, true, true);
+        if (blinkAtAwake)
+            isOpen = true;
+    }
+
+    // 也可以在OnPostRender()中更新
+    private void Update()
+    {
+        if (isOpen && TargetMaterial != null )
+        {
+            if (timer.Duration != blinkTime)
+                timer.Reset(blinkTime);
+            TargetMaterial.SetColor("_Color", new Color(color.r,color.g,color.b, maxColorAlpha * blinkCurve.Evaluate(Mathf.PingPong(timer.GetPercent() * 2F,1))));
+
+            for (int j = 0; j < meshFilters.Length; j++)
+                Graphics.DrawMesh(meshFilters[j].sharedMesh, meshFilters[j].transform.localToWorldMatrix, TargetMaterial, 0);   // 对选中物体再次渲染。
+        }
+    }
+}
---
+++ Assets/Items/Prop/Shader/DrawOnlyColor.shader
@@ -0,0 +1,36 @@
+﻿
+Shader "Custom/DrawOnlyColor" {
+	Properties {
+		_Color ("Color", Color) = (0, 0, 0, 1)
+	}
+    SubShader {
+		Tags { "RenderType"="Opaque" "Queue"="Geometry"}
+		
+		Pass {
+			
+			ZWrite OFF							// 关闭深度写入
+			Blend SrcAlpha OneMinusSrcAlpha		// 透明混合颜色
+			
+			CGPROGRAM
+			
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+			
+			fixed4 _Color;
+             
+            float4 vert (float4 vertex : POSITION) : SV_POSITION
+            {
+                return UnityObjectToClipPos(vertex);
+            }
+
+			half4 frag() :COLOR {
+				return _Color;
+			}
+			ENDCG
+		}
+		
+	}
+	FallBack OFF
+}
