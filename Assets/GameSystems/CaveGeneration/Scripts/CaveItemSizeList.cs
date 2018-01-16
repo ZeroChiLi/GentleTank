afd1796a8f967a3b913591626f52a58633febff8
ChiLi_Lin <904981663@163.com>
2018-01-16 12:59:42 +0800

    Add CaveItemSizeList.

---
+++ Assets/GameSystems/CaveGeneration/Scripts/CaveItemSizeList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameSystem/CaveItemSizeList")]
+public class CaveItemSizeList : ScriptableObject 
+{
+    [System.Serializable]
+    public struct CaveItem
+    {
+        public GameObject item;
+        public Vector3 size;
+        public Vector3 offset;
+    }
+
+    public List<CaveItem> caveItemList;
+
+}
