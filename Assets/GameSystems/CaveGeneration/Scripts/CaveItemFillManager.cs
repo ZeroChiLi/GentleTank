b4b0626bc0e6eb03f8ddf1686e87209c6df65195
ChiLi_Lin <904981663@163.com>
2018-01-22 10:14:50 +0800

    Add CaveItemFillManager.

---
+++ Assets/GameSystems/CaveGeneration/Scripts/CaveItemFillManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CaveItemFillManager : MonoBehaviour 
+{
+    public CaveItemList mainItemList;
+    public CaveItemList groundItemList;
+    public CaveItemList fillItemList;
+
+    public void SetItems(CaveRegion region)
+    {
+
+    }
+
+
+}
