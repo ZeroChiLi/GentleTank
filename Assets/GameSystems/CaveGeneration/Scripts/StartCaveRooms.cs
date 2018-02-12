4198a3bd5224b79353c0fa1814ae663862b8d4f6
ChiLi_Lin <904981663@163.com>
2018-02-12 14:53:53 +0800

    Update.

--- Assets/GameSystems/CaveGeneration/Scripts/StartCaveRooms.cs
+++ Assets/GameSystems/CaveGeneration/Scripts/StartCaveRooms.cs
@@ -3,11 +3,22 @@ using UnityEngine;
 
 public class StartCaveRooms : MonoBehaviour
 {
-    public List<CaveRoomEditor> rooms;
+    public MapGenerator map;
+    public List<CaveRoomEditor> rooms = new List<CaveRoomEditor>();
 
     private List<CaveRoom> caveRooms;
+    private Color green = new Color(0, 1, 0, 0.3f);
+    private Color red = new Color(1, 0, 0, 0.4f);
 
     private void OnDrawGizmos()
     {
+        if (map)
+        {
+            Gizmos.color = green;
+            Gizmos.DrawCube(Vector3.zero, new Vector3(map.width, 1, map.height));
+        }
+        Gizmos.color = red;
+        for (int i = 0; i < rooms.Count; i++)
+            Gizmos.DrawCube(rooms[i].pos, GameMathf.Vec2ToVec3XZ(rooms[i].size));
     }
 }
