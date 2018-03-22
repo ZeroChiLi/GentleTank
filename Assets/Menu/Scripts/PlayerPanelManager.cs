9af1548ddf2d8f6c13a752f6e413f529601983bf
ChiLi_Lin <904981663@163.com>
2018-03-22 14:15:58 +0800

    Add PlayerPanelManager.

---
+++ Assets/Menu/Scripts/PlayerPanelManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class PlayerPanelManager : MonoBehaviour 
+{
+    public bool isEnter;
+    public Text enterText;
+    public string enterKey;
+    public UnityEvent OnPlayerEnter;
+
+    public void Awake()
+    {
+        enterText.text = string.Format("按“{0}”加入", enterKey);
+    }
+
+    public void Update()
+    {
+        if (!isEnter && Input.GetButtonDown(enterKey))
+        {
+            isEnter = true;
+            OnPlayerEnter.Invoke();
+        }
+    }
+
+}
