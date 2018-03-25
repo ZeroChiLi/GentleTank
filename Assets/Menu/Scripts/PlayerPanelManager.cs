07d5f68fd78b4f2954f75901b926dded3b875de3
ChiLi_Lin <904981663@163.com>
2018-03-25 16:50:47 +0800

    Update PlayerPanel.

--- Assets/Menu/Scripts/PlayerPanelManager.cs
+++ Assets/Menu/Scripts/PlayerPanelManager.cs
@@ -4,19 +4,26 @@ using UnityEngine.UI;
 
 public class PlayerPanelManager : MonoBehaviour 
 {
+    [System.Serializable]
+    public sealed class InputButtons
+    {
+        public string enterButton = "Fire";
+        public string horizontalButton = "Horizontal";
+        public string verticalButton = "Vertical";
+    }
     public bool isEnter;
     public Text enterText;
-    public string enterKey;
+    public InputButtons inputsButton;
     public UnityEvent OnPlayerEnter;
 
     public void Awake()
     {
-        enterText.text = string.Format("按“{0}”加入", enterKey);
+        enterText.text = string.Format("按“{0}”加入\n按左右键切换坦克\n按上键切换颜色", inputsButton.enterButton);
     }
 
     public void Update()
     {
-        if (!isEnter && Input.GetButtonDown(enterKey))
+        if (!isEnter && Input.GetButtonDown(inputsButton.enterButton))
         {
             isEnter = true;
             OnPlayerEnter.Invoke();
