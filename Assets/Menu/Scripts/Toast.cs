1f34cbd767ee617be95731972a3b6fc622982c56
ChiLi_Lin <904981663@163.com>
2018-04-05 12:33:38 +0800

    Update Toast.

--- Assets/Menu/Scripts/Toast.cs
+++ Assets/Menu/Scripts/Toast.cs
@@ -6,8 +6,14 @@ public class Toast : MonoBehaviour
     static public Toast Instance { get; private set; }
     public Text toastText;          // 文本信息
     public float duration = 3f;
+    public AnimationCurve moveCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    public float moveDuration = 1f;
+    public Vector3 startPos = new Vector3(0, -32, 0);
+    public Vector3 endPos = new Vector3(0, 64, 0);
 
+    private RectTransform rectTransform;
     private CountDownTimer timer;
+    private CountDownTimer moveTimer;
 
     public CountDownTimer Timer
     {
@@ -18,7 +24,9 @@ public class Toast : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+        rectTransform = GetComponent<RectTransform>();
         gameObject.SetActive(false);
+        moveTimer = new CountDownTimer(moveDuration, false, false);
     }
 
     /// <summary>
@@ -26,6 +34,11 @@ public class Toast : MonoBehaviour
     /// </summary>
     private void Update()
     {
+        if (!moveTimer.IsTimeUp)
+            rectTransform.localPosition = Vector3.LerpUnclamped(startPos, endPos, moveCurve.Evaluate(moveTimer.GetPercent()));
+        else
+            rectTransform.localPosition = endPos;
+
         if (Timer.IsTimeUp)
         {
             Timer.Reset(duration, true);
@@ -43,6 +56,7 @@ public class Toast : MonoBehaviour
         this.duration = duration;
         toastText.text = content;
         Timer.Start();
+        moveTimer.Start();
         gameObject.SetActive(true);
         Debug.Log(content);
     }
