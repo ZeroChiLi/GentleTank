eac82c9b174d17c6e1751fd42e907851515923de
ChiLi_Lin <904981663@163.com>
2018-04-14 17:29:11 +0800

    Add MapSelectionManager.

---
+++ Assets/Menu/Scripts/MapSelectionManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MapSelectionManager : MonoBehaviour
+{
+    public Dropdown dropdown;
+    public Image mapPreview;
+    public string[] sceneNames;
+
+    public void SelectCurrentMap()
+    {
+        mapPreview.sprite = dropdown.options[dropdown.value].image;
+    }
+
+    public void LoadSelectedScene()
+    {
+        SceneManager.LoadScene(sceneNames[dropdown.value]);
+    }
+
+}
