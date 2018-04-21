8641678c36f1a3b15cffc9e9d94beedbcb2e0d2b
ChiLi_Lin <904981663@163.com>
2018-04-21 15:14:36 +0800

    Add GameInformation.

---
+++ Assets/Main/Main Scripts/GameInformation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct PlayerInfo
+{
+    public int id;
+    public bool isAI;
+    public int selectedIndex;
+    public Color color;
+
+    public PlayerInfo(int id, bool isAI,int selectedIndex,Color color)
+    {
+        this.id = id;
+        this.isAI = isAI;
+        this.selectedIndex = selectedIndex;
+        this.color = color;
+    }
+};
+
+public class GameInformation : MonoBehaviour
+{
+    // 四位玩家的信息
+    public PlayerInfo[] players = new PlayerInfo[4]{
+        new PlayerInfo ( 0, false, 0, Color.blue ),
+        new PlayerInfo ( 1, false, 1, Color.green),
+        new PlayerInfo( 2, false, 2, Color.red),
+        new PlayerInfo( 3, false, 3, Color.yellow),
+    };
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+}
---
+++ Assets/Menu/Scripts/SettingMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    public void UpdateVolumes()
+    {
+        audioMixer.SetFloat("SFXVolume", LogarithmicDbTransform(Mathf.Clamp01(sfxSlider.value)));
+        audioMixer.SetFloat("MusicVolume", LogarithmicDbTransform(Mathf.Clamp01(musicSlider.value)));
+    }
+
+    protected static float LogarithmicDbTransform(float volume)
+    {
+        volume = (Mathf.Log(89 * volume + 1) / Mathf.Log(90)) * 80;
+        return volume - 80;
+    }
+}
